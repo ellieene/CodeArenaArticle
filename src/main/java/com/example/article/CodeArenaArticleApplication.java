5188459216f219541c549dad16abb9da59db0ccf
ellieene <ellieene@student.21-school.ru>
2025-08-12 16:26:16 +0700

    create CodeArenaArticle

---
+++ src/main/java/com/example/article/CodeArenaArticleApplication.java
@@ -0,0 +1,13 @@
+package com.example.article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeArenaArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeArenaArticleApplication.class, args);
+	}
+
+}
